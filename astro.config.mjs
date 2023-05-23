aeddf41b233bac4da726405a8a02ebbadac4ee1f
Gianfranco Trotta <giantrotta@me.com>
2023-05-22 22:44:00 -0400

    update favicon, add root styles, update meta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
-import compress from "astro-compress";
+import compress from 'astro-compress';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), compress({
-    css: true,
-    html: {
-      removeAttributeQuotes: false
-    },
-    img: false,
-    js: true,
-    svg: false,
-    logger: 1
-  })]
+  integrations: [
+    tailwind(),
+    compress({
+      css: true,
+      html: {
+        removeAttributeQuotes: false,
+      },
+      img: false,
+      js: true,
+      svg: false,
+      logger: 1,
+    }),
+  ],
 });
